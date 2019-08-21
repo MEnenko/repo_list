c74450890131df143e9fc9404c67bdf44a618735
Maksym Yenenko <m.enenko@gmail.com>
2019-08-21 20:34:35 +0300

    add action types

---
+++ src/actions/types.ts
@@ -0,0 +1,5 @@
+export const GET_REPOS = 'GET_REPOS';
+export const GET_REPOS_ERROR = 'GET_REPOS_ERROR';
+export const GET_BRANCHES_ERROR = 'GET_BRANCHES_ERROR';
+export const GET_BRANCHES = 'GET_BRANCHES';
+
