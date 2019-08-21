24b7297e1f1d111f254abf31dec3b82f2673c4c8
Maksym Yenenko <m.enenko@gmail.com>
2019-08-21 20:34:01 +0300

    add reducer branch

---
+++ src/reducers/branch.ts
@@ -0,0 +1,29 @@
+import { GET_BRANCHES } from "../actions/types";
+import { Action, Reducer } from 'redux';
+import {IBranch} from '../types';
+
+export interface IRepoBranchState {
+  readonly list: IBranch[]
+}
+
+interface IGetRepoBranches extends Action<typeof GET_BRANCHES> {
+  list: IBranch[]
+}
+
+const initialState: IRepoBranchState = {
+  list: []
+};
+
+export const reducer: Reducer<IRepoBranchState, IGetRepoBranches> = (state = initialState, action) => {
+  switch (action.type) {
+    case GET_BRANCHES:
+      return {
+        ...state,
+        list: action.list
+      };
+    default:
+      return state;
+  }
+};
+
+export default reducer;
