81bb7664e9fe81834c23feeefbc4094dfe4b4ab0
Maksym Yenenko <m.enenko@gmail.com>
2021-03-11 13:57:04 +0200

    hide service worker

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
-import * as serviceWorker from './serviceWorker';
+// import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-serviceWorker.unregister();
+// serviceWorker.unregister();
